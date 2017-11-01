fb6edb1e2d961af85351fb5889fe5a8d2f2d12a9
Amit Kushwaha <amit_kushwaha@outlook.com>
2017-11-02 01:38:49 +0530

    readme update and imports too

---
+++ README.md
@@ -0,0 +1,10 @@
+## To run on standalone
+ ```bash
+bash run.sh standalone # Make sure workers are running
+```
+
+
+## To run on Cluster mode. Used docker based cluster addition and running on master container
+ ```bash
+bash run.sh cluster # Make sure related filepaths available in docker container
+```
--- recipe_difficulty_extractuion.py
+++ recipe_difficulty_extractuion.py
@@ -2,15 +2,11 @@
 import os
 import re
 
-from pyspark import SparkConf
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
-from pyspark.sql.functions import struct
-from pyspark.sql.functions import udf
-from pyspark.sql.types import StringType
-
 EASY_MAX_TIM = 30
 MIN_HARD_TIME = 60
+EASY = 'Easy'
+HARD = 'Hard'
+UNDEFINED = 'Undefined'
 
 
 def time_in_minutes(time_in_string_format):
@@ -26,19 +22,30 @@ def time_in_minutes(time_in_string_format):
 
 
 def is_recipe_difficult(row):
-    all_times = [time_in_minutes(time_in_string_format) for time_in_string_format in row]
     try:
+
+        all_times = [time_in_minutes(time_in_string_format) for time_in_string_format in row]
+        if not any(all_times):
+            return UNDEFINED
         if sum(all_times) <= EASY_MAX_TIM:
-            return 'Easy'
+            return EASY
         elif sum(all_times) > MIN_HARD_TIME:
-            return 'Hard'
-        return 'Undefined'
+            return HARD
+        return UNDEFINED
     except:
-        return 'Undefined'
+        return UNDEFINED
 
 
 if __name__ == '__main__':
+    from pyspark import SparkConf
+    from pyspark.sql import SparkSession
+    from pyspark.sql.functions import col
+    from pyspark.sql.functions import struct
+    from pyspark.sql.functions import udf
+    from pyspark.sql.types import StringType
+
     recipes_dir = './recipes-etl'
+
     conf = SparkConf().setAppName('DifficultyColumnAdd')
     spark = SparkSession \
         .builder \
